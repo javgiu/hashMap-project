bcb8125082ea481a7d59e5a64ec7ac891ad13468
javgiu <javypicallo@gmail.com>
2025-11-04 11:30:56 -0500

    Add Math.abs to hash to prevent negative values

--- hash-map.js
+++ hash-map.js
@@ -16,7 +16,7 @@ class HashMap {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
-      bucket = hashCode % this.capacity;
+      bucket = Math.abs(hashCode) % this.capacity;
     }
     return { hashCode, bucket };
   }
